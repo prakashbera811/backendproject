0e9d957abc4a6acbf88b1276b56c26cfefb8dd4a
berap4111 <berap4111@gmail.com>
2025-12-16 21:15:33 +0530

    first controller,router

--- src/app.js
+++ src/app.js
@@ -11,4 +11,10 @@ app.use(express.json({limit : "25kb"}));
 app.use(express.urlencoded({extended : true , limit : "25kb"}));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+//user router import
+import router from "./routes/user.routes.js";
+app.use("/api/v1/user",router)
+
+
 export { app };
