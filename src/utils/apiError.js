993727a0aa53011da8ff7aa52615f6cb23592fbd
berap4111 <berap4111@gmail.com>
2025-12-16 13:58:55 +0530

    cloudinary & multer

--- src/utils/apiError.js
+++ src/utils/apiError.js
@@ -3,7 +3,7 @@ class apiError extends Error {
         statuscode ,
         message ="something went wrong",
         errors = [],
-        statck = ""
+        stack = ""
 
     ){
         super(message);
@@ -13,8 +13,8 @@ class apiError extends Error {
         this.success = false;
         this.message = message;
 
-        if(statck){
-            this.stack = statck;
+        if(stack){
+            this.stack = stack;
         }else{
             Error.captureStackTrace(this , this.constructor);
         }
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,23 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+// Configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET, // Click 'View API Keys' above to copy your API secret
+});
+
+
+const uploadOnCloudinary = (localFilePath) => {
+  try {
+    if(!localFilePath) return null;
+    const response = cloudinary.uploader.upload(localFilePath, {resource_type:"auto" })
+    //file is uploaded on cloudinary
+    console.log("file is uploaded on cloudinary" ,response.url )
+  } catch (error) {
+    fs.unlinkSync(localFilePath) // remove the locally saved temporary as uploaded operation  got failed
+  }
+};
+
+export {uploadOnCloudinary}
